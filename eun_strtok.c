41bb8a2de8da7f16ee75132480c9de2e99229e2e
Eun-Min <peunmin717@naver.com>
2018-12-11 22:54:24 +0900

    eun_strtok.c
    to implement strtok function.
    Strtok is ..

---
+++ eun_strtok.c
@@ -0,0 +1,30 @@
+//2014046326 EunMin Pak - Department of English Language and Culture
+//Assignment of strtok
+
+#include <stdio.h>
+#include <string.h>
+
+char * strtok(char* str1, const char* str2);
+
+int main(void){
+    char *sen;
+    char *a;
+    
+    printf("enter the sentence :");
+    gets_s(sen,40);
+    
+    printf("enter the character to distinguish the sentence : ");
+    a = getch();
+    
+    return 0;
+}
+
+char * strtok(char* str1, const char* str2){
+  int i;
+  char* newarr;
+  char* final = NULL;
+  
+  for(){
+  }
+  
+}
