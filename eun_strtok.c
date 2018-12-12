0b0d7b8b993c29f0f6aed0f708ca6736ef3422bf
Eun-Min <peunmin717@naver.com>
2018-12-12 12:15:53 +0900

    Update eun_strtok.c

--- eun_strtok.c
+++ eun_strtok.c
@@ -7,15 +7,14 @@
 char * strtok(char* str1, const char* str2);
 
 int main(void){
-    char *sen;
-    char *tok;
+    char sen[50];
+    char tok='.';
     
     printf("enter the sentence : ");
     scanf("%s", sen);
     //get the sentence
-    
-    printf("enter the character to distinguish the sentence : ");
-    scanf("%c",tok);
+   
+    printf("%s",strtok(sen,&tok));
     
     return 0;
 }
@@ -26,21 +25,28 @@ char * strtok(char* str1, const char* str2){
   char *tmp;
   
   for(i=0; i<strlen(str1); i++){
-      if(*str1==*str2){   // found token!
-            *str1 = '\0';
-             count++;     // count null
+      str1 = str1+i;  //move the pointer
+          
+      if(*(str1+i)==*str2){   // compare the token and replace token to '\0'
+            *(str1+i) = '\0';
+             count++;         // count null
       }
   }
+// line 27 - 35 , the process to replace token to '\0'
   
-      for(i=0; count!=0; i++){ // if count goes 0, terminate
-        str1 = str1+i;
-        tmp = str1+i;  // cuz it's the pointer.. don't need like this  :  tmp + i = str1+i
+      for(i=0; count>0; i++){ // if count goes 0, it means no more words -> terminate
+        str1 = str1+i;         // as moving the pointer
+     
+          // caution : in the case of the pointer.. don't need like this  :  tmp + i = str1+i
            
         if(*str1=='\0'){
             count--;
-            return tmp;  
+            str1 = str1;       //again
+            for(j=0; j!='\0'; j++){
+                printf("%s",str1+j);
+            }
         }
-   }
+    }
     
 }
 
